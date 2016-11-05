a5fc05dc504eab3663c23c5bccd0f3d5f87ecd06
unknown <1584377884@qq.com>
2016-11-05 23:18:43 +0800

    commit all modify files

--- hello.cpp
+++ hello.cpp
@@ -1,4 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
 void main(){
-	printf("hello world\n");
+	FILE *p;
+	if((p=fopen("D:/test/Cproject/test.txt","ab"))==NULL){
+		printf("can not open the file\n");
+		exit(0);
+	}
+	fwrite("hello",1,5,p);
+	//fclose("
+
 }
